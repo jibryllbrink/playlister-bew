4c5abbd7cc05c6587194a274057e3d5c5c6df27c
jibryllbrink <jibryllbrink@icloud.com>
2019-12-08 17:32:03 -0800

    Users can see all playlists

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     """Return homepage."""
-    return 'Flask is Cool!!'
+    return 'Flask is Cool!!'    
     return render_template('home.html', msg='Flask is Cool!!')
+
+
+
+
+
+playlists = [
+    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+]
+@app.route('/playlists')
+def playlists_index():
+    """Show all playlists."""
+
+    return render_template('playlists_index.html', playlists=playlists)
 
 if __name__ == '__main__':
     app.run(debug=True)
