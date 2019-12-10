417a5a35e7911bcd22ca7ff30854c2da22a105cc
jibryllbrink <jibryllbrink@icloud.com>
2019-12-09 21:57:35 -0800

    Fixed pymongo

--- app.py
+++ app.py
@@ -26,6 +26,11 @@ def playlists_index():
     """Show all playlists."""
 
     return render_template('playlists_index.html', playlists=playlists.find())
+
+@app.route('/playlists/new')
+def playlists_new():
+    """Create a new playlist."""
+    return render_template('playlists_new.html')
     
 if __name__ == '__main__':
     app.run(debug=True)
